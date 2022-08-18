988b1152185f6143758f039bc913985fc3fcd84a
TbishopP <TbishopP@users.noreply.github.com>
2022-08-18 09:41:50 +0000

    Fix styling

--- src/Models/Choice.php
+++ src/Models/Choice.php
@@ -8,9 +8,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Pentangle\LaravelQuestionnaire\Database\Factories\ChoiceFactory;
 use Pentangle\LaravelQuestionnaire\Enums\InputTypeEnum;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
-use Spatie\MediaLibrary\HasMedia;
 
 class Choice extends Model implements HasMedia
 {
@@ -40,7 +40,7 @@ class Choice extends Model implements HasMedia
         'question_id',
         'image',
         'extras',
-        'associated_value'
+        'associated_value',
     ];
 
     /**
