16c8454ed3196c36bcb3b8237089e8cbde352910
TbishopP <TbishopP@users.noreply.github.com>
2022-08-24 16:14:20 +0000

    Fix styling

--- src/QuestionableTrait.php
+++ src/QuestionableTrait.php
@@ -4,18 +4,15 @@ namespace Pentangle\LaravelQuestionnaire;
 
 use Pentangle\LaravelQuestionnaire\Models\Choice;
 use Pentangle\LaravelQuestionnaire\Models\Instance;
-use Pentangle\LaravelQuestionnaire\Models\Question;
-use Pentangle\LaravelQuestionnaire\Models\Response;
 
 trait QuestionableTrait
 {
     public function createNewInstance(): int
     {
         return $this->instances()->create([
-            'user_id' => auth()->id()
+            'user_id' => auth()->id(),
         ])->id;
     }
-
 
     public function instances()
     {
