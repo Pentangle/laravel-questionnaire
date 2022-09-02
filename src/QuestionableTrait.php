3ceecb3efae513e3eb061f796a06d53c6dd22f50
TbishopP <100126472+TbishopP@users.noreply.github.com>
2022-09-02 13:05:46 +0100

    - remove response = null

--- src/QuestionableTrait.php
+++ src/QuestionableTrait.php
@@ -10,6 +10,10 @@ trait QuestionableTrait
 {
     public function createNewInstance(): int
     {
+        //if not logged in show error message
+        if (!auth()->check()) {
+            throw new \Exception('You must be logged in to create a new instance');
+        }
         return $this->instances()->create([
             'user_id' => auth()->id(),
         ])->id;
@@ -20,7 +24,7 @@ trait QuestionableTrait
         return $this->morphMany(Instance::class, 'participant');
     }
 
-    public function respondToQuestion(Instance $instance = null, Response $response = null, Choice $choice = null, string $text = '')
+    public function respondToQuestion(Instance $instance = null, Choice $choice = null, string $text = '')
     {
         $instance->responses()->create([
             'text' => $text,
